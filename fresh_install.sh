e66f9c3973cdc7137421f3a9b795bdbee019268a
zannis <zannis.kal@gmail.com>
2015-09-12 07:57:39 +0300

    init

---
+++ fresh_install.sh
@@ -0,0 +1,32 @@
+###############################################################################
+#                                                                             #
+# This is a script to ease the process of a fresh Archbang Linux installation #
+#                                                                             #
+###############################################################################
+
+# Fix the mirrorlist to include only Greek mirrors
+
+sudo mv /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.original
+wget "https://www.archlinux.org/mirrorlist/?country=GR&protocol=https&ip_version=4" -O ~/mirrors
+sudo sed 's/^..//' ~/mirrors > /etc/pacman.d/mirrorlist
+rm ~/mirrors
+
+# Remove archassault repository
+sudo head -n -4 /etc/pacman.conf >> /etc/pacman.conf
+
+# Install archlinuxfr repository
+sudo printf "%s\n%s\n%s\n" "[archlinuxfr]" "SigLevel = Never" "Server = http://repo.archlinux.fr/$arch" >> /etc/pacman.conf
+
+# Update repositories and installed applications
+sudo pacman -Syu
+
+# Replace bash with zsh
+sudo pacman -S zsh --noconfirm
+chsh -s $(which zsh)
+
+# Install necessary applications
+sudo pacman -S --noconfirm yaourt vim xclip i3 chromium base-devel feh qbittorrent skype
+
+# Install from AUR
+yaourt -S --noconfirm oh-my-zsh 
+
